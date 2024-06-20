d0e18da9452467a3dcb79376e516e9607f1f9d5a
Mateus Marchinack <167545286+Marchinack@users.noreply.github.com>
2024-06-20 15:14:59 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+System.out.println("Insira sua altura:");
+        double altura = sc.nextDouble();
+System.out.println("Seu peso:");
+        double massa = sc.nextDouble();
+System.out.println("Seu inc é:");
+        double imc = massa / (altura * altura);
+        System.out.printf("Seu imc é %.2f", imc);
+        if (imc < 18.5) {
+            System.out.println(" Abaixo do peso");
+        } else if (imc >= 18.5 && imc < 25) {
+            System.out.println(" Peso normal");
+        } else if (imc >= 25 && imc < 30) {
+            System.out.println(" Sobrepeso");
+        } else if (imc >= 30) {
+            System.out.println(" Obesidade");
+        } 
+    }
+}
